4cfa89257703d37f618abe5c148402fc74afa438
Hannah Moran <hannahmoran@gmail.com>
2018-12-18 21:50:50 -0800

    clean up wcv code; clean up notebooks

--- src/vis/wordcloud_vis.py
+++ src/vis/wordcloud_vis.py
@@ -17,12 +17,12 @@ def remove_key_tokens(dictionary, n, no_above, no_below=0, keep_n=100000):
     Creates a list of custom stopwords for the wordcloud - ingests a gensim corpora dictionary
     from an LDA model and removes the top n most frequent appearing words, as well as words
     that appear in more than no_abov (%) of the reviews. 
-    :param object dictionary: the term frequency dictionary for the LDA model
+    :param object dictionary: the term frequency dictionary for the LDA model (from gensim)
     :param int n: how many of the most frequent terms to remove
     :param float no_abov: the percentage of reviews in which a term must appear to be removed
 
     This function does not modify the dictionary, but just returns a list of words to be used
-    as stopwords by the wordcloud
+    as stopwords by the wordcloud. 
 
     """
     save = set(['GOODREVIEW', 'BADREVIEW', 'VGOODREVIEW', 'VBADREVIEW'])
@@ -36,13 +36,16 @@ def remove_key_tokens(dictionary, n, no_above, no_below=0, keep_n=100000):
 
     no_above_abs = int(no_above * dictionary.num_docs)  # convert fractional threshold to absolute threshold
 
+    
     code_ids = []
+    # add in any code IDs that appear in the dictionary to a list to save
     for t in ['GOODREVIEW', 'BADREVIEW', 'VGOODREVIEW', 'VBADREVIEW']:
         try:
             code_ids.append(dictionary.token2id[t])
         except KeyError:
             continue
 
+    # keep IDs if they pass the threshold or are part of the code ID list for this dict
     good_ids = (
         v for v in itervalues(dictionary.token2id)
         if no_below <= dictionary.dfs.get(v, 0) <= no_above_abs
@@ -52,14 +55,26 @@ def remove_key_tokens(dictionary, n, no_above, no_below=0, keep_n=100000):
         good_ids = good_ids[:keep_n]
     bad_words = [dictionary[id] for id in set(dictionary).difference(good_ids)]
     remove_words = list(set(bad_words) | set(most_frequent_words))
-    remove_words.extend(['GOODREVIEW', 'BADREVIEW', 'VGOODREVIEW', 'VBADREVIEW'])
+    
+    # toggle this line on or off to remove or keep the codewords in the wordclouds
+    #remove_words.extend(['GOODREVIEW', 'BADREVIEW', 'VGOODREVIEW', 'VBADREVIEW'])
+    
     #print('discarding %i tokens: %s...', len(dictionary) - len(good_ids), bad_words[:10])
     #print('keeping %i tokens which were in no less than %i and no more than %i (=%.1f%%) documents',
     #    len(good_ids), no_below, no_above_abs, 100.0 * no_above)
     return remove_words
 
 def get_wordcloud_removewords(df, final_results, product, input_text):
+    """
+    Returns a list of custom stopwords for the wordcloud afer identifying how and which words
+    should be removed.
+    :param df DataFrame: the full dataframe of reviews
+    :param final_results DataFrame: the dataframe specifying the tuned LDA model parameters
+    :param product str: the product to make wordclouds for
+    :param input_text str: the type of input (vanilla, coded, valence coded)
 
+    This function returns a list of words to be used as stopwords by the wordcloud
+    """
     tn = final_results.loc[product, 'top_n removed']
     na = final_results.loc[product, 'n_above threshold']
     data = df[df['ProductId']==product]
@@ -69,10 +84,26 @@ def get_wordcloud_removewords(df, final_results, product, input_text):
     return remove_words
 
 def generate_wordcloud(df, final_results, product, topic, encoding_type, input_text):
+    """
+    Generates a wordcloud for a particular product, topic, input text combination. 
+    Word frequency is determined by how often the word appears in the actual reviews
+    for this product and topic combination. 
+    :param df DataFrame: the full dataframe of reviews
+    :param final_results DataFrame: the dataframe specifying the tuned LDA model parameters
+    :param product str: the product to make wordclouds for
+    :param topic int: the topic number
+    :param encoding_type: the type of text input
+    :param input_text str: the type of input (vanilla, coded, valence coded)
+
+    This function returns a wordcloud image
+    """
+
+    # filters out products where the fit value is below 0.7
     data = df.loc[(df['ProductId']==product)&
                   (df['{} Topic'.format(encoding_type)]==topic)&
                   (df['{} Fit'.format(encoding_type)]>=0.7)]
     if len(data>0):
+        # only make a wordcloud if there were a non-zero number of reviews in this combination
         remove_words = get_wordcloud_removewords(df, final_results, product, input_text)
         texts = " ".join(review for review in data[input_text])
         wordcloud = WordCloud(stopwords=remove_words,
@@ -82,14 +113,23 @@ def generate_wordcloud(df, final_results, product, topic, encoding_type, input_t
                              random_state=42).generate(texts)
         return wordcloud
     else:
-        print('No wordcloud')
+        print('Error - No wordcloud')
         return None
 
 
 def get_params(df, product, encoding_type):
+    """
+    Gets the parameters of the tuned LDA model for a particular product-topic-encoding combination.
+    These are necessary to determine which token IDs to remove from the dict for the wordcloud. 
+    :param df DataFrame: the full dataframe of reviews
+    :param product str: the product to make wordclouds for
+    :param encoding_type: the type of text input (Vanilla, Coded, Valence Coded)
+
+    This function returns the a tuple of a list of topics, and the number of topics in the tuned LDA model
+    """
     data = df.loc[(df['ProductId']==product)&
                   (df['{} Fit'.format(encoding_type)]>=0.7)]
-    topics = data.loc[df['ProductId']==product, '{} Topic'.format(encoding_type)].unique()
+    topics = list(data['{} Topic'.format(encoding_type)].unique())
     n_topics=len(topics)
     return topics, n_topics
 
@@ -111,6 +151,18 @@ def do_plot(fig, df, product, t, topic_list, encoding_type, gap, final_results, 
     
 def make_all_wordclouds(product, df, vanilla_final_results,
 						coded_final_results, valence_final_results):
+    """
+    Plots all wordclouds for a product, using the words from reviews to build the wordclouds vs.
+    the topic model word distribution.
+    :param df DataFrame: the full dataframe of reviews
+    :param product str: the product to make wordclouds for
+    :param vanilla_final_results DataFrame: dataframe of tuned vanilla LDA model information
+    :param coded_final_results DataFrame: dataframe of tuned coded LDA model information
+    :param valence_final_results DataFrame: dataframe of tuned valence LDA model information
+
+
+    This function plots a grid of wordclouds.
+    """
 
     vanilla_topics, n_vanilla_topics = get_params(df, product, 'Vanilla')
     coded_topics, n_coded_topics = get_params(df, product, 'Coded')
@@ -169,6 +221,18 @@ def make_all_wordclouds(product, df, vanilla_final_results,
 
 def make_model_wordclouds_review_counts(product, df, vanilla_final_results,
 										coded_final_results, valence_final_results):
+    """
+    Plots all wordclouds for a product, using the topic model term distribution as the frequency dictionary,
+    but filtering to topics with at least one matching review and including the review counts. 
+    :param df DataFrame: the full dataframe of reviews
+    :param product str: the product to make wordclouds for
+    :param vanilla_final_results DataFrame: dataframe of tuned vanilla LDA model information
+    :param coded_final_results DataFrame: dataframe of tuned coded LDA model information
+    :param valence_final_results DataFrame: dataframe of tuned valence LDA model information
+
+
+    This function plots a grid of wordclouds.
+    """
     vanilla_dicts = get_frequency_dicts(df, vanilla_final_results, product, 'clean_vanilla', 'vanilla_outputs')
     coded_dicts = get_frequency_dicts(df, coded_final_results, product, 'clean_coded', 'coded_outputs')
     valence_dicts = get_frequency_dicts(df, valence_final_results, product, 'clean_valence', 'valence_outputs')
@@ -246,8 +310,19 @@ def make_model_wordclouds_review_counts(product, df, vanilla_final_results,
     # Fix up the layout - this removes extra whitespace after the supertitle
     spec.tight_layout(fig, rect=[0, 0.03, 1, 0.97])
 
-
 def get_frequency_dicts(df, final_results, product, input_text, load_path):
+    """
+    Gets frequency dictionaries for a particular product and input text combination.
+    :param df DataFrame: the full dataframe of reviews
+    :param product str: the product to make wordclouds for
+    :param final_results DataFrame: dataframe of tuned LDA model information
+    :param input_text str: name of column of final_results to use to build frequency dictionary
+    :param load_path str: where to get the tuned LDA model from
+
+
+    This function returns a dictionary of dictionaries - one word frequency dictionary for each topic
+    belonging to the product and input text type, according to the tuned LDA model. 
+    """
     # Load in the tuned LDA model for the product and input text combo
     t = final_results.loc[product, 'num_topics']
     p = final_results.loc[product, 'passes']
@@ -267,6 +342,18 @@ def get_frequency_dicts(df, final_results, product, input_text, load_path):
     return freq_dicts
 
 def do_model_plot(freq_dicts, t, topic_list, gap, plot_column, fig):
+    """
+    Plots a single wordcloud for a topic and input text type.
+    :param freq_dicts dict: a dict of dictionaries, each one containing word frequencies for a topic
+    :param t int: the index position of the topic in the topic list
+    :param topic_list list: the list of topics
+    :param gap int: the difference between the number of topics in this topic list and the max # of topics for the product
+    :param plot_column int: the column position for this plot in the figure grid
+    :param spec GridSpec: the matplotlib gridspec for this figure
+
+
+    This function plots a single wordcloud into a grid.
+    """
     ax = fig.add_subplot(spec[t+gap+1, plot_column])
     cloud = WordCloud(background_color='white',
                      random_state=42).generate_from_frequencies(freq_dicts[t])
@@ -276,6 +363,18 @@ def do_model_plot(freq_dicts, t, topic_list, gap, plot_column, fig):
     
 def make_model_wordclouds(product, df, vanilla_final_results,
 							coded_final_results, valence_final_results):
+    """
+    Plots all model wordclouds for a product,  meaning the frequency dictionaries of the topic models
+    are what drives the visual
+    :param df DataFrame: the full dataframe of reviews
+    :param product str: the product to make wordclouds for
+    :param vanilla_final_results DataFrame: dataframe of tuned vanilla LDA model information
+    :param coded_final_results DataFrame: dataframe of tuned coded LDA model information
+    :param valence_final_results DataFrame: dataframe of tuned valence LDA model information
+
+
+    This function plots a grid of wordclouds.
+    """
 
     vanilla_dicts = get_frequency_dicts(df, vanilla_final_results, product, 'clean_vanilla', 'vanilla_outputs')
     coded_dicts = get_frequency_dicts(df, coded_final_results, product, 'clean_coded', 'coded_outputs')
@@ -297,108 +396,8 @@ def make_model_wordclouds(product, df, vanilla_final_results,
     coded_gap = max_topics - n_coded_topics
     valence_gap = max_topics - n_valence_topics
     
-    # Heights ratio controls relative height of subplots
-    # I'm using a blank row of plots at the top to label the columns
-    heights = [0.02]
-    heights.extend([1]*max_topics)
-    if max_topics>5:
-        fig = plt.figure(figsize=[15, 3*max_topics])
-    else:
-        fig = plt.figure(figsize=[15, 15])
-    spec = gridspec.GridSpec(ncols=3, nrows=max_topics+1, figure=fig, wspace=0.1, hspace=0.1,
-                            height_ratios=heights)
-
-    fig.suptitle("Topic Model Wordclouds for Product {}".format(product), fontsize=14, fontweight='bold')
-    ax_vanilla = fig.add_subplot(spec[0, 0])
-    ax_vanilla.set_title("Vanilla Inputs", fontsize=12, fontweight='bold')
-    ax_vanilla.axis('off')
-    ax_coded = fig.add_subplot(spec[0, 1])
-    ax_coded.set_title("Coded Inputs",  fontsize=12, fontweight='bold')
-    ax_coded.axis('off')
-    ax_valence = fig.add_subplot(spec[0, 2])
-    ax_valence.set_title("Valence Inputs",  fontsize=12, fontweight='bold')
-    ax_valence.axis('off')
-
-    for t in range(n_vanilla_topics):
-        ax = fig.add_subplot(spec[t+vanilla_gap+1, 0])
-        cloud = WordCloud(background_color='white',
-                         random_state=42).generate_from_frequencies(vanilla_dicts[t])
-        ax.imshow(cloud, interpolation='bilinear')
-        ax.set_title("Topic {}".format(vanilla_topics[t]))
-        ax.axis('off')
-
-    
-    for t in range(n_coded_topics):
-        ax = fig.add_subplot(spec[t+coded_gap+1, 1])
-        cloud = WordCloud(background_color='white',
-                         random_state=42).generate_from_frequencies(coded_dicts[t])
-        ax.imshow(cloud, interpolation='bilinear')
-        ax.set_title("Topic {}".format(coded_topics[t]))
-        ax.axis('off')
-        
-    for t in range(n_valence_topics):
-        ax = fig.add_subplot(spec[t+valence_gap+1, 2])
-        cloud = WordCloud(background_color='white',
-                         random_state=42).generate_from_frequencies(valence_dicts[t])
-        ax.imshow(cloud, interpolation='bilinear')
-        ax.set_title("Topic {}".format(valence_topics[t]))
-        ax.axis('off')    
-    # Fix up the layout - this removes extra whitespace after the supertitle
-    spec.tight_layout(fig, rect=[0, 0.03, 1, 0.97])
-
-
-def get_frequency_dicts(df, final_results, product, input_text, load_path):
-    # Load in the tuned LDA model for the product and input text combo
-    t = final_results.loc[product, 'num_topics']
-    p = final_results.loc[product, 'passes']
-    tn = final_results.loc[product, 'top_n removed'].astype(int)
-    na = final_results.loc[product, 'n_above threshold']
-
-    lda = gensim.models.ldamodel.LdaModel.load('../models/{}/final_models/{}_{}_{}_{}_{}'.format(load_path, 
-                                                                                         product, 
-                                                                                         t, p,
-                                                                                      tn, na))
-    freq_dicts = {}
-    for topic in range(0,t):
-        freqs = lda.show_topic(topic, topn=100)
-        # the above returns a list of tuples of the top n words in the topic
-        freq_dict = dict(freqs) 
-        freq_dicts[topic] = freq_dict
-    return freq_dicts
-
-def do_model_plot(freq_dicts, t, topic_list, gap, plot_column, fig):
-    ax = fig.add_subplot(spec[t+gap+1, plot_column])
-    cloud = WordCloud(background_color='white',
-                     random_state=42).generate_from_frequencies(freq_dicts[t])
-    ax.imshow(cloud, interpolation='bilinear')
-    ax.set_title("Topic {}".format(topic_list[t]))
-    ax.axis('off')
-    
-def make_model_wordclouds(product, df, vanilla_final_results,
-							coded_final_results, valence_final_results):
-
-    vanilla_dicts = get_frequency_dicts(df, vanilla_final_results, product, 'clean_vanilla', 'vanilla_outputs')
-    coded_dicts = get_frequency_dicts(df, coded_final_results, product, 'clean_coded', 'coded_outputs')
-    valence_dicts = get_frequency_dicts(df, valence_final_results, product, 'clean_valence', 'valence_outputs')
-
-    vanilla_topics = list(vanilla_dicts.keys())
-    n_vanilla_topics = len(vanilla_topics)
-    
-    coded_topics = list(coded_dicts.keys())
-    n_coded_topics = len(coded_topics)
-    
-    valence_topics = list(valence_dicts.keys())
-    n_valence_topics = len(valence_topics)
-    
-
-    max_topics = max(n_vanilla_topics, n_coded_topics, n_valence_topics)
-
-    vanilla_gap = max_topics - n_vanilla_topics
-    coded_gap = max_topics - n_coded_topics
-    valence_gap = max_topics - n_valence_topics
-    
-    # Heights ratio controls relative height of subplots
-    # I'm using a blank row of plots at the top to label the columns
+    # Use heights ratio to control relative height of subplots
+    # Create a blank row of plots at the top to label the columns
     heights = [0.02]
     heights.extend([1]*max_topics)
     if max_topics>5:
@@ -444,8 +443,6 @@ def make_model_wordclouds(product, df, vanilla_final_results,
         ax.imshow(cloud, interpolation='bilinear')
         ax.set_title("Topic {}".format(valence_topics[t]))
         ax.axis('off')    
-    # Fix up the layout - this removes extra whitespace after the supertitle
+    # Fix up the layout - remove extra whitespace under the supertitle
     spec.tight_layout(fig, rect=[0, 0.03, 1, 0.97])
-    
-
     
